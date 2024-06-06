b6e595991afe5da70f18fa79b6399420a02f4fe6
BeneArp <benedetta.arpino@gmail.com>
2024-06-06 15:20:14 +0200

    prove condizione numeri divisibili sia per 3 che per 5

--- js/script.js
+++ js/script.js
@@ -16,6 +16,9 @@ for(let i = 1; i <= 100; i++){
     }else if (i % 5 === 0){
         numero = "Buzz";
     }
+    else if ((i % 3 === 0) && (i % 5 === 0)){
+        numero = "FizzBuzz";
+    }
 
     console.log(numero);
 }
